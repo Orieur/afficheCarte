b1a8d470da184f16c3e6e04fc19b0d14b2d324a0
Orieur <46268976+Orieur@users.noreply.github.com>
2019-01-01 14:53:35 +0100

    Update setup.py

--- .gitignore/setup.py
+++ .gitignore/setup.py
@@ -2,11 +2,11 @@
 from setuptools import setup
 
 setup(
-    name='afficheCarte',
+    name='run',
     version='1.0.0',
     url='https://github.com/Orieur/afficheCarte',
     license='BSD',
-    description='Heroku environment variable configurations for Flask',
+    description='affiche une carte',
     long_description=__doc__,
     zip_safe=False,
     include_package_data=True,
